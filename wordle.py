9db1de03b9bc5611514ce791539fb6f91bcd131a
kalikabo <kalikabo@hu-berlin.de>
2022-08-06 23:37:10 +0200

    added english wordlist set

--- wordle.py
+++ wordle.py
@@ -2,16 +2,19 @@ import colorama
 import random
 from colorama import Fore
 from english_words import *
+from nltk.corpus import words
 
-secret_word_list = ['monkey', 'browed', 'strait', 'cobble', 'dabber', 'amazed', 'export', 'ripper', 'easier', 'astral']
+secret_word_list = words.words()
 
 
 def guessgame():
     secret_word = random.choice(secret_word_list)
+    secret_word.lower()
     i = 5
     while i > 0:
         tracker = []
         guess = input("Enter guess: ")
+
         if len(guess) != len(secret_word):
             print(f"Word is {len(secret_word)} chars long")
         elif guess not in english_words_alpha_set and guess not in secret_word_list: #fixed useless termination when incorrect length occurs
@@ -25,22 +28,21 @@ def guessgame():
             count_s = 0
             for letter_j in guess:
                 if letter_j == secret_word[letter_k]:
-                    print(Fore.GREEN + letter_j + Fore.RESET)
+                    print(Fore.LIGHTGREEN_EX + letter_j + Fore.RESET)
                 elif letter_j in secret_word:
                     count_i = guess.count(letter_j)
                     count_s = secret_word.count(letter_j)
                     if count_s == count_i:
-                        print(Fore.YELLOW + letter_j + Fore.RESET)
+                        print(Fore.LIGHTYELLOW_EX + letter_j + Fore.RESET)
                     elif count_s != count_i and letter_j not in tracker:
                         print(Fore.LIGHTYELLOW_EX + letter_j + Fore.RESET)
                         tracker.insert(0, letter_j)
                     elif count_s != count_i and letter_j in tracker:
-                        print(Fore.RED + letter_j + Fore.RESET)
+                        print(Fore.LIGHTRED_EX + letter_j + Fore.RESET)
                         tracker.insert(0, letter_j)
                 else:
                     print(Fore.RED + letter_j + Fore.RESET)
                 letter_k += 1
-            print(tracker)
             i -= 1
     print(f"Out of tries! Word was {secret_word}")
 
